828b6e9d97b764b4e0a767e08cb94a99448e7d9b
esau-morais <esaumorais7@gmail.com>
2024-05-18 22:25:49 -0300

    test(linear-search): prevent false positive

--- src/linear-search/linear-search.spec.ts
+++ src/linear-search/linear-search.spec.ts
@@ -3,11 +3,13 @@ import { linearSearch } from "./linear-search";
 
 test("linear search", () => {
   const arr = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
+  const arr2 = ["hello", "hell", "hel", "he", "h"];
 
-  expect(linearSearch(arr, 69)).toBeTrue;
-  expect(linearSearch(arr, 1336)).toBeFalse;
-  expect(linearSearch(arr, 69420)).toBeTrue;
-  expect(linearSearch(arr, 69421)).toBeFalse;
-  expect(linearSearch(arr, 1)).toBeTrue;
-  expect(linearSearch(arr, 0)).toBeFalse;
+  expect(linearSearch(arr, 69)).toEqual(true);
+  expect(linearSearch(arr, 1336)).toEqual(false);
+  expect(linearSearch(arr, 69420)).toEqual(true);
+  expect(linearSearch(arr, 69421)).toEqual(false);
+  expect(linearSearch(arr, 1)).toEqual(true);
+  expect(linearSearch(arr, 0)).toEqual(false);
+  expect(linearSearch(arr2, "1")).toEqual(false);
 });
